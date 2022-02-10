c992d780062e08cd47265b99d78281f85cee9212
Nazarenko-Dmtr <97964675+Nazarenko-Dmtr@users.noreply.github.com>
2022-02-10 13:35:17 +0200

    Add files via upload

---
+++ Lesson_8_task_2_var_2_Nazarenko_Dmytro.py
@@ -0,0 +1,7 @@
+﻿# Lesson_8_task_2_Var_2_Nazarenko_Dmytro
+
+from random import randrange 
+list_1 = set([randrange(3, 100, 3) for _ in range (30)])
+list_2 = set([randrange(5, 100, 5) for _ in range (20)])
+
+print (list_1 & list_2)
