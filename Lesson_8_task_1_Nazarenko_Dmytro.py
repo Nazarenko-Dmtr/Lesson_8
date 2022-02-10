2a1d8f5d2bcf1bdd3523212d17ae814c610ae011
Nazarenko-Dmtr <97964675+Nazarenko-Dmtr@users.noreply.github.com>
2022-02-10 09:57:29 +0200

    Add files via upload

---
+++ Lesson_8_task_1_Nazarenko_Dmytro.py
@@ -0,0 +1,9 @@
+﻿# Lesson_8_task_1_Nazarenko_Dmytro
+
+line_1 = input("Print 1st line: ")
+line_2 = input("Print 2nd line: ")
+
+line_1 = set("".join(line_1.split()))
+line_2 = set("".join(line_2.split()))
+
+print (line_1 & line_2)
---
+++ Lesson_8_task_2_Nazarenko_Dmytro.py
@@ -0,0 +1,9 @@
+﻿# Lesson_8_task_2_Nazarenko_Dmytro
+
+list_1 = []
+list_2 = []
+for i in range (3, 100, 3):
+    list_1.append(i)
+for j in range (5, 100, 5):
+    list_2.append(j)
+print (set(list_1) & set(list_2))
